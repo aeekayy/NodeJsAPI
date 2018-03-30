335b901e4a344c2aace4817db728abc505a7ea9c
EC2 Default User <ec2-user@ip-172-26-9-6.us-west-2.compute.internal>
2018-03-30 03:02:14 +0000

    Adding images to the backend.

---
+++ migrations/201803291952-create-image.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+	defaultValue: Sequelize.UUIDV4
+      },
+      owner_id: { type: Sequelize.UUID, allowNull: false },
+      image_url: { type: Sequelize.STRING }, 
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Images');
+  }
+};
---
+++ models/image.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Image = sequelize.define('Image', {
+    id: {
+       type: DataTypes.UUID, 
+       primaryKey: true,
+       defaultValue: DataTypes.UUIDV4
+    },
+    owner_id: { type: DataTypes.UUID }, 
+    image_url: { type: DataTypes.STRING },
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    },
+    hooks: {
+    },
+    instanceMethods: {
+    }
+  });
+
+  return Image;
+};
